382436e7e2b44df12d573fff73a5d5d48d56a133
Tom Dent <tomdent991@hotmail.com>
2024-05-25 14:21:20 +0200

    first 32 mins

---
+++ app/(tabs)/trips.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text>Trips</Text>
+    </View>
+  )
+}
+
+export default index
